678916fea3bf879046cc353930d127101b27b71d
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-05-05 23:12:21 +0300

    added comments

--- src/app/services/my-apis.service.ts
+++ src/app/services/my-apis.service.ts
@@ -35,69 +35,91 @@ export class MyAPIsService {
   constructor(private http: HttpClient) {
 
   }
+
+
+  //api that checks if a user is in the database whenever a user attempts to log in
   checkUser(user: User){
     return this.http.post(this.url + 'logIn.php', user);
   }
+
+  //signing up api, it adds the newly registered user to the database
   addUser(user: User){
     return this.http.post(this.url + 'SignUp.php', user);
   }
 
 
+
+  //retrieves user info to be used in other pages, some are saved in the local storage to be used as parameters in other apis (such as user_id)
   getUserInfo(email : string) {
     return this.http.get(this.url + 'getUserInfo.php?email='+email);
 
   }
 
 
+
+  //api to add a task to the todo list
   addTask(id : any, task : string) {
     return this.http.get(this.url + 'addTask.php?id='+id+'&task='+task);
 
   }
 
   
+  //api that displays the previously added tasks to the screen
   getTasks(id : any) {
     return this.http.get(this.url + 'getTasks.php?id='+id);
 
   }
 
+
+  //api responsible of deleting a task when a user crosses it.
   deleteTask(id : any ){
     return this.http.get(this.url + 'deleteTask.php?id='+id);
 
   }
 
+
+  //This api returns the number of quotes in the database to know the range within which the system should load a random number
+  //to generate a random quote everyday
   getQuotesNumber(){
     return this.http.get(this.url + 'getQuotesNumber.php');
 
   }
 
 
+  //Retrieves a random quote whenever a user logs in to the application.
   getRandomQuote(id){
     return this.http.get(this.url + 'getRandomQuote.php?id='+id);
 
   }
 
 
-  
+  //same as getQuotesNumber above but for songs
   getSongsNumber(){
     return this.http.get(this.url + 'getSongsNumber.php');
 
   }
 
-
+//same as getRandomQuote above but for songs
   getRandomSong(id){
     return this.http.get(this.url + 'getRandomSong.php?id='+id);
 
   }
 
+
+  //post request that adds a goal form the user to the database
   addGoal(goal: Goal){
     return this.http.post(this.url + 'addGoal.php', goal);
   }
 
+
+  //displays previously added goals to the screen
   getGoals(id : any) {
     return this.http.get(this.url + 'getGoals.php?id='+id);
 
   }
 
+
+  //displays previously added milestones to the screen
   getMilestones(id : any) {
     return this.http.get(this.url + 'getMilestones.php?id='+id);
 
