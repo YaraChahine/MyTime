5768a206fdce23aa790cfbbd7994653574995e61
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-05-05 23:18:05 +0300

    adding comments

--- src/app/pages/log-in/log-in.page.ts
+++ src/app/pages/log-in/log-in.page.ts
@@ -14,7 +14,10 @@ export class LogInPage implements OnInit {
   isSubmitted = false;
   ionicForm: FormGroup;
   constructor(private router: Router,private service: MyAPIsService,public alertController: AlertController,public formBuilder: FormBuilder) { }
+ 
+ 
   ngOnInit() {
+    //the following form builder ensures the validation of the user's input
     this.ionicForm = this.formBuilder.group({
       Email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       Password: ['', [Validators.required]],
@@ -23,9 +26,15 @@ export class LogInPage implements OnInit {
   get errorControl() {
     return this.ionicForm.controls;
   }
+
+
+  //if a user click son signup the following function routes the user to the sign up page
   public signup(){
     this.router.navigate(['sign-up']);
 }
+
+
+//this functions is called when the user attempts to log in with wrong credentia;s
 async showAlert() {
   const alert = await this.alertController.create({
     header: 'Attention',
@@ -34,18 +43,27 @@ async showAlert() {
   });
    await alert.present();
 } //form: NgForm
+
+
+
 public onSubmit(){
   this.isSubmitted = true;
   if(!this.ionicForm.valid){
+
     console.log('Please provide all the required values!');
       return false;
   }else{
+
+    //when a user logs in, if the credentials entered do not correspond to any user in the db,
+    //the function showAlert is called; 
   const user = this.ionicForm.value;
   this.service.checkUser(user).subscribe(response => {
     if(response==null){
         this.showAlert();
       }
       else{
+        //else, we save the logge in user's email in the local storage (as a unique key that can be used in future apis)
+        //and the user is routed to the application.
          localStorage.setItem("logged-in-email",this.ionicForm.value.Email);
         console.log(this.ionicForm.value.Email);
         this.router.navigate(['tabs']);
